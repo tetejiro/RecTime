f8a2f2207fbbda1b649ecab4daf546c0f5e6a92f
tetejiro <hy1733505@gmail.com>
2024-01-25 21:35:49 +0900

    月移動（日付のバグはあり）

--- RecordTimes/app/src/main/java/com/example/RecordTime/MainActivity.java
+++ RecordTimes/app/src/main/java/com/example/RecordTime/MainActivity.java
@@ -1,61 +1,22 @@
 package com.example.RecordTime;
 
 import androidx.fragment.app.FragmentActivity;
-import androidx.room.Room;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.EditText;
-
-import com.example.RecordTime.Rooms.Database;
-import com.example.RecordTime.Rooms.TimeTable;
-import com.example.RecordTime.Rooms.TimeTable_Dao;
-import com.example.RecordTime.Rooms.TimeTable_RoomEntity;
-
-import java.util.List;
 
 
 public class MainActivity extends FragmentActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // 月画面フラグメントを表示
-        getSupportFragmentManager().beginTransaction()
-                .setReorderingAllowed(true)//トランザクションに関与するフラグメントの状態変更を最適化
-                .add(R.id.month_fragment_container, new MonthFragment())
-                .commit();
-    }
-
-    // 新規登録　開始
-    public void registration_start(View view) {
-        EditText editText = findViewById(R.id.contents);
-//        editText.getText();
-        try {
-            Log.d("aaaaaaaaaa=====>", "aaaaaaaaaaaa");
-            Database DB = Room.databaseBuilder(getApplicationContext(),
-                    Database.class, "TimeTable").build();
-            TimeTable_Dao dao = DB.timeTable_dao();
-            Log.d("bbbbbbb=====>", "bbbbbbbbbb");
-
-            dao.deleteAll();
-            dao.insert(new TimeTable_RoomEntity("inserted title=======>"));
-            Log.d("all========>", dao.getAll().get(0).title);
-
-//            Timely_RecyclerView_Adapter adapter = new Timely_RecyclerView_Adapter();
-//            adapter.init();
-        } catch (Exception e) {
-            Log.d("FAILUR=====>", "registration_start failed");
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)//トランザクションに関与するフラグメントの状態変更を最適化
+                    .add(R.id.month_fragment_container, new MonthFragment())
+                    .commit();
         }
-    }
-
-    // 新規登録　終了
-    public void registration_end(View view) {
-        EditText editText = findViewById(R.id.contents);
-        Log.d("registration_end==>", editText.getText().toString());
     }
 }
--- RecordTimes/app/src/main/java/com/example/RecordTime/MonthFragment.java
+++ RecordTimes/app/src/main/java/com/example/RecordTime/MonthFragment.java
@@ -1,11 +1,11 @@
 package com.example.RecordTime;
 
-import android.icu.util.Calendar;
-import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,10 +15,24 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MonthFragment extends Fragment {
+    View view;
+    Calendar this_month_instance;
+    int this_first_day_of_week;
+    Integer this_last_day_of_week;
+    int preMaxDate;
+    int rest;
 
-    private ArrayList<DateList> dateLists;
+    private ArrayList<DateList> dateList = new ArrayList<>();
+    Calendar cl = new GregorianCalendar();
+    int this_year = cl.get(Calendar.YEAR);
+    int this_month = cl.get(Calendar.MONTH);
+    int this_date = cl.get(Calendar.DATE);
+    int this_maxDate = cl.getActualMaximum(Calendar.DATE); // 今月の最大日数
+    int idx = 0;
 
     @Override
     //container: fragment が挿入される親
@@ -31,70 +45,107 @@ public class MonthFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onStart();
 
+        this.view = view;
+
+        Button next = view.findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(this_month == 11) {
+                    this_month = 0;
+                    this_year++;
+                } else {
+                    this_month++;
+                }
+                viewCalendar();
+            }
+        });
+
+        Button prev = view.findViewById(R.id.prev);
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(this_month == 0) {
+                    this_month = 11;
+                    this_year--;
+                } else {
+                    this_month--;
+                }
+                viewCalendar();
+            }
+        });
+
+        viewCalendar();
+    }
+
+    public void viewCalendar() {
         dataInitialize();
 
+        // 〇年を今年にする
+        TextView yearText = this.view.findViewById(R.id.year);
+        yearText.setText(this_year + "年");
         // 〇月を今月にする
-        Integer month = this.dateLists.get(0).getMonth() + 1;
-        TextView tx = view.findViewById(R.id.month);
-        tx.setText(month + "月");
+        TextView monthText = this.view.findViewById(R.id.month);
+        monthText.setText(this_month + 1 + "月");
 
         // RecyclerView を生成して各日にちを生成
-        RecyclerView recyclerView = view.findViewById(R.id.date_recycler_view_container);
+        RecyclerView recyclerView = this.view.findViewById(R.id.date_recycler_view_container);
         recyclerView.setHasFixedSize(true);
         // ①recycleView に layoutManager をセット
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 7));
         // ②Adapter を生成して RecyclerView にセット
-        recyclerView.setAdapter(new Monthly_RecyclerView_Adapter(getActivity(), this.dateLists));
+        recyclerView.setAdapter(new Monthly_RecyclerView_Adapter(getActivity(), this.dateList));
     }
 
-    // dateLists = [DateList, , ...] を作成
+    // dateList = [DateList, , ...] を作成
     private void dataInitialize() {
-        this.dateLists = new ArrayList<>(); // push する対象
 
-        Calendar cl = new GregorianCalendar();
-        int maxDate = cl.getActualMaximum(Calendar.DATE); // 今月の最大日数
-        int year = cl.get(Calendar.YEAR);
-        int month = cl.get(Calendar.MONTH);
+        this.dateList.clear(); // push する対象
 
+        this.idx = 0;
         // 今月1日のインスタンス
-        Calendar today = new GregorianCalendar(year, month, 1);
+        this.this_month_instance = new GregorianCalendar(this.this_year, this.this_month, 1);
         // 今月1日の曜日
-        int first_day_of_week = today.get(Calendar.DAY_OF_WEEK);
+        this.this_first_day_of_week = this_month_instance.get(Calendar.DAY_OF_WEEK);
         // 今月最終日の曜日
-        int last_day_of_week = new GregorianCalendar(year, month, maxDate).get(Calendar.DAY_OF_WEEK);
+        this.this_last_day_of_week = new GregorianCalendar(this.this_year, this.this_month, this.this_maxDate).get(Calendar.DAY_OF_WEEK);
         // 先月の最大日数
-        int preMaxDate = new GregorianCalendar(year, month-1, 1)
+        this.preMaxDate = new GregorianCalendar(this.this_year, this.this_month-1, 1)
                                     .getActualMaximum(Calendar.DATE);
 
-        int[] weekDays = {0, 0, 1, 2, 3, 4, 5, 6};
-
-        // 日数分(とその前後)の日付クラスを作って、配列に add する。
-        this.dateLists = new ArrayList<>();
-        int idx = 0;
-        // （マイナス）1日よりも前に表示する日数
-        int before_first_day = - weekDays[first_day_of_week];
-
-        // 1日よりも前の日付生成
-        if(before_first_day <= 0) {
-            while(before_first_day < 0) {
-                this.dateLists.add(new DateList(year, month - 1, preMaxDate + before_first_day + 1));
-                before_first_day++;
+        //★① 1日よりも前の日付生成
+        this.this_first_day_of_week = - (this.this_first_day_of_week - 1); // -(今月初日の曜日 - 1)
+        if(this.this_first_day_of_week <= 0) {
+            //「1日の曜日-1」が付け足す先月の日数。その日数の絶対値をマイナスにして、カウントアップし、0未満の場合は繰り返す。
+            while(this.this_first_day_of_week < 0) {
+                this.dateList.add(new DateList(this.this_year, this.this_month - 1, this.preMaxDate+this.this_first_day_of_week+1, false));
+                this.this_first_day_of_week++;
             }
-            idx++;
+            this.idx++;
         }
-        // その月の日付生成
-        while(idx <= maxDate) {
-            this.dateLists.add(new DateList(year, month, idx));
-            idx++;
+
+        //★② その月の日付生成
+        while(this.idx <= this.this_maxDate) {
+            // 当日は当日フラグ = true
+            if(this.this_year == 2024 && this.idx == this.this_date) {
+                this.dateList.add(new DateList(this.this_year, this.this_month, this.idx, true));
+                Log.d("11111====>", String.valueOf(this.idx));
+                // 当日は当日フラグ = false
+            } else {
+                this.dateList.add(new DateList(this.this_year, this.this_month, this.idx, false));
+                Log.d("22222====>", String.valueOf(this.idx));
+            }
+            this.idx++;
         }
-        // 来月の日付生成
-        int rest = weekDays[last_day_of_week] + 1;
-        idx = 1;
-        // 先月の最終週との合計　14日分の日付を生成する。
-        while (14 - rest > 0) {
-            this.dateLists.add(new DateList(year, month + 1, idx));
-            idx++;
-            rest++;
+
+        //★③ 来月の日付生成
+        this.rest = 7 - this.this_last_day_of_week;
+        this.idx = 1;
+        // 先月の最終週の日数（rest） + 次の週の日数（7日）
+        while (rest+7 > 0) {
+            this.dateList.add(new DateList(this.this_year, this.this_month + 1, this.idx, false));
+            this.idx++;
+            this.rest--;
         }
     }
 
@@ -102,11 +153,13 @@ public class MonthFragment extends Fragment {
         private int year; // 年
         private int month;// 月
         private int date; // 日にち
+        private boolean todayFlg; // 今日かどうかのフラグ
 
-        DateList(int year,int month, int date) {
+        DateList(int year,int month, int date, boolean todayFlg) {
             this.year = year;
             this.month = month;
             this.date = date;
+            this.todayFlg = todayFlg;
         }
 
         public int getYear() {
